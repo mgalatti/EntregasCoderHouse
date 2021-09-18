ddf8e77f310d2884cab316d528a56ba78522331f
Mauro Galatti <mauro.galatti@globant.com>
2021-09-18 12:44:08 -0300

    entrega-1

--- app.js
+++
@@ -1,7 +0,0 @@
-let num1 = prompt("Ingrese un numero")
-let numParsed = Number(num1)
-
-let num2 = prompt("Ingrese un segundo numero")
-let num2Parsed = Number(num2)
-
-alert(`Usted ingreso ${num1} y ${num2} y el producto de esos numeros es ${numParsed * num2Parsed}`)
